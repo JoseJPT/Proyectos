3368c5da0587ab7c7c5868bbbec1146977e1f482
Joss <jjpt1129@gmail.com>
2025-06-11 19:53:22 -0600

    Amazon track

--- python/scrypt_keyboard.py
+++ python/scrypt_keyboard.py
@@ -5,7 +5,13 @@ import pyautogui as pa
 #Linea para ver dimensiones
 #print (pa.size())
 
-#print(pa.position()) Posicion del mouse
+#print(pa.position()) #Posicion del mouse
+#pa.click(x=433,y=464)
+#pa.typewrite("Holaaaa")
+#for i in range (10):
+ #   pa.moveTo(x=random.randint(10,1910), y=random.randint(10,1910))
 
-for i in range (10):
+while True:
     pa.moveTo(x=random.randint(10,1910), y=random.randint(10,1910))
+    if keyboard.is_pressed('q'):
+        break
